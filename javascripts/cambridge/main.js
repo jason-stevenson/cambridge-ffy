76f04aba7cd446b5844074fe55ecd11b452f00b4
Jason Stevenson <jason@artofmultimedia.com.au>
2014-06-16 08:32:10 +1000

    updated panels

--- javascripts/cambridge/main.js
+++ javascripts/cambridge/main.js
@@ -6,29 +6,31 @@ $( document ).ready(function() {
 	// As we cant use the body element to apply the cover images to
 	// we need to re-size the internal div to fill the browser window
 
-	$('.chapter-cover').css({'height':($(window).height())});
+	// $('.chapter-cover').css({'height':($(window).height())});
 
-	$(window).resize(function(){
-    $('.chapter-cover').css({'height':($(window).height())});
-    });
-
-
-
-	// Panels
-	$('.activity-panel .download a').html('<span class="icon-doc-solid"></span><div>Download</div>');
-	$('.end-of-chapter .download a').html('<span class="icon-doc-solid"></span><div>Download</div>');
+	// $(window).resize(function(){
+ //    $('.chapter-cover').css({'height':($(window).height())});
+ //    });
 
 
 
-	// Coloured bullets
-	$( ".end-of-chapter ul" ).each(function( index ) {
-	  $('.end-of-chapter ul li').wrapInner('<span class="item"/>');
-	  $(".end-of-chapter .item").css("color", "black");
-	});
-	$( ".end-of-chapter ol" ).each(function( index ) {
-	  $('.end-of-chapter ol li').wrapInner('<span class="item"/>');
-	  $(".end-of-chapter .item").css("color", "black");
-	});
+	// // Panels
+	 $('.investigate-panel').wrapInner('<div class="content"></div>');
+
+
+	// $('.activity-panel .download a').html('<span class="icon-doc-solid"></span><div>Download</div>');
+	// $('.end-of-chapter .download a').html('<span class="icon-doc-solid"></span><div>Download</div>');
+
+
+
+	// // Coloured bullets
+
+	// $( ".end-of-chapter ul" ).each(function( index ) {
+	//   $('.end-of-chapter ul li').wrapInner('<span class="item"/>');
+	//   $(".end-of-chapter .item").css("color", "black");
+	// });
+
+	 $('ol li').wrapInner('<span class="item"/></span>');
 
 
 });
